3c76ac97d7aff01cfe55fb63582cab61414c1ace
Vladislav Yotkov <vladislav.yotkov@student.manchester.ac.uk>
2023-04-15 08:55:05 +0300

    make rouge maximising strategy
    Former-commit-id: 85d0a4203a41e7e62e74d91bdcab4d6fb44c5dfb

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,17 +1,19 @@
 import json
 
+import numpy as np
 import pandas as pd
 import torch
 from datasets import Dataset
+from rouge import Rouge
 from scipy.special import softmax
 from tqdm import tqdm
 from transformers import BertTokenizer, Trainer, BertForSequenceClassification, BertConfig, TrainingArguments
 
+from baseline_summarizers import get_baseline_summary
 from extractor import batch_str_to_batch_tensors, FinRNN
 from metrics import calc_rouge
 from preprocessing import clean
 from query import get_all_summaries, get_embedding_model, get_report
-from baseline_summarizers import get_baseline_summary
 
 args = TrainingArguments(
     output_dir='../tmp/',
@@ -35,7 +37,42 @@ def trim_string(text, max_length=1000):
         return text
 
 
-def select_summary_sents(probabilities, sentences, max_sents=int(1000/25)):
+def rouge_max_top_n_sentences(summary_sentences, reference_summary: str, n: int = 40):
+    """
+    Extract the top-N sentences from a list of summary sentences based on their ROUGE-2 scores and sort them in their natural order.
+
+    Parameters:
+    -----------
+    summary_sentences (List[str]): A list of candidate summary sentences.
+    reference_summary (str): The reference document summary used to compute the ROUGE-2 scores.
+    n (int, optional): The number of top sentences to extract (default: 40).
+
+    Returns:
+    --------
+    List[str]: A list of the top-N summary sentences sorted in their natural order.
+    """
+    # Initialize the Rouge object
+    rouge = Rouge()
+
+    # Compute the ROUGE-2 scores for each sentence in summary_sentences
+    rouge_2_scores = [rouge.get_scores(sentence, reference_summary, avg=True)["rouge-2"]["f"] for sentence in summary_sentences]
+
+    # Get the indices that would sort the sentences by their ROUGE-2 scores in descending order
+    sorted_indices = np.argsort(rouge_2_scores)[::-1]
+
+    # Select the top-N sentence indices based on the ROUGE-2 scores
+    top_n_indices = sorted_indices[:n]
+
+    # Re-sort the top-N sentence indices in their natural order
+    top_n_indices_natural_order = sorted(top_n_indices)
+
+    # Extract the top-N summary sentences in their natural order
+    summary_sentences_top = [summary_sentences[i] for i in top_n_indices_natural_order]
+
+    return summary_sentences_top
+
+
+def select_summary_sents(probabilities, sentences, max_sents=int(1000/25), rouge_maximisation=False, reference_summary=None):
     """
     Selects a summary of the input sentences based on the predicted probabilities of label 1.
 
@@ -43,6 +80,8 @@ def select_summary_sents(probabilities, sentences, max_sents=int(1000/25)):
         probabilities (numpy.ndarray): Array of predicted probabilities with shape (N, 2).
         sentences (List[str]): List of input sentences.
         max_sents (int, optional): Maximum number of sentences to include in the summary. Defaults to 40.
+        rouge_maximisation (bool, optional): Whether to maximise sentence-level ROUGE-2 score. Defaults to True.
+        reference_summary (str, optional): Reference summary to use for ROUGE-2 maximisation. Defaults to None.
 
     Returns:
         summary (str): The selected summary.
@@ -54,10 +93,13 @@ def select_summary_sents(probabilities, sentences, max_sents=int(1000/25)):
     # Filter the tensor for rows where label 1 is predicted
     summary_probs = predictions[summary_indices]
     summary_sentences = [sentences[i] for i in summary_indices]
-    # Get the top-k probabilities and corresponding sentence indices
-    summary_probs_top, summary_indices_top = torch.topk(summary_probs[:, 1], k=min(max_sents, summary_probs.shape[0]))
-    # Get the top-k sentences based on the selected indices
-    summary_sentences_top = [summary_sentences[i] for i in summary_indices_top]
+    if rouge_maximisation and reference_summary is not None:
+        summary_sentences_top = rouge_max_top_n_sentences(summary_sentences, reference_summary, n=min(max_sents, len(summary_sentences)))
+    else:
+        # Get the top-k probabilities and corresponding sentence indices
+        summary_probs_top, summary_indices_top = torch.topk(summary_probs[:, 1], k=min(max_sents, summary_probs.shape[0]))
+        # Get the top-k sentences based on the selected indices
+        summary_sentences_top = [summary_sentences[i] for i in summary_indices_top]
     # Join the selected sentences to form the summary
     summary = " ".join(summary_sentences_top)
     # Ensure that the summary is less than 1000 words
@@ -129,7 +171,7 @@ def load_model_transformer(model_dir, device='cpu'):
     return model_transformer
 
 
-def evaluate_model(model, config, embedding_model=None):
+def evaluate_model(model, config, embedding_model=None, rouge_maximisation=False):
     """
         Evaluates the model on the test set. The model is evaluated using the ROUGE evaluation metric.
     """
@@ -162,14 +204,15 @@ def evaluate_model(model, config, embedding_model=None):
             print(outputs)
             predictions = softmax(outputs.predictions, axis=1)
             print(predictions)
-        generated_summary = select_summary_sents(predictions, sentences)
 
         # Calculate ROUGE scores for each summary compared to the generated summary
         rouge_scores_per_summary = []
         gold_summaries_dict = get_all_summaries(file_id=report_id, training=False)
         for _, gold_summary in gold_summaries_dict.items():
             try:
-                rouge_score = calc_rouge(generated_summary, clean(gold_summary).lower())
+                gold_summary_ = clean(gold_summary).lower()
+                generated_summary = select_summary_sents(predictions, sentences, rouge_maximisation=rouge_maximisation, reference_summary=gold_summary_)
+                rouge_score = calc_rouge(generated_summary, gold_summary_)
             except ValueError:
                 rouge_score = {'rouge-l': {'f': 0}, 'rouge-1': {'f': 0}, 'rouge-2': {'f': 0}}
             rouge_scores_per_summary.append(rouge_score)
@@ -229,7 +272,8 @@ def evaluate_baseline(config, max_sents=int(1000/25)):
         rouge_scores_per_summary = []
         gold_summaries_dict = get_all_summaries(file_id=report_id, training=False)
         for _, gold_summary in gold_summaries_dict.items():
-            rouge_score = calc_rouge(generated_summary, clean(gold_summary).lower())
+            gold_summary_ = clean(gold_summary).lower()
+            rouge_score = calc_rouge(generated_summary, gold_summary_)
             rouge_scores_per_summary.append(rouge_score)
         # Get the maximum ROUGE-L score from the list of scores per summary
         max_rouge_l_score = get_max_rouge_l_score(rouge_scores_per_summary)
@@ -247,9 +291,9 @@ def evaluate_baselines(configs, max_sents=int(1000/25)):
         rouge_scores_list.append(rouge_scores)
         df = rouge_dict_to_df(rouge_scores)
         df.to_csv(f"{c['model_name']}_rouge_scores.csv")
-    # Save the list of dictionaries to a file
-    with open(f"{c['model_name']}_rouge_scores.json", "w") as outfile:
-        json.dump(rouge_scores_list, outfile)
+        # Save the list of dictionaries to a file
+        with open(f"{c['model_name']}_rouge_scores.json", "w") as outfile:
+            json.dump(rouge_scores_list, outfile)
 
 
 def unpack_rouge_metric(d, key='f'):
@@ -285,24 +329,24 @@ def rouge_dict_to_df(data):
     return df
 
 
-def evaluate_models(configs, embedding_model=None, device='cpu'):
+def evaluate_models(configs, embedding_model=None, rouge_maximisation=True, device='cpu'):
     rouge_scores_list = []
     for c in tqdm(configs, desc='Evaluating models'):
         print(f"{c['model_type']}")
         rouge_scores = None
         if c['model_type'] == 'transformer':
             model_transformer = load_model_transformer(c['model_dir'], device=device)
-            rouge_scores = evaluate_model(config=c, model=model_transformer, embedding_model=None)
+            rouge_scores = evaluate_model(config=c, model=model_transformer, embedding_model=None, rouge_maximisation=rouge_maximisation)
         elif c['model_type'] == 'gru':
             model_rnn = FinRNN(hidden_size=c['hidden_size'])
             model_rnn.load_state_dict(torch.load(c['model_path'], map_location=torch.device(device)), strict=False)
-            rouge_scores = evaluate_model(config=c, model=model_rnn, embedding_model=embedding_model)
+            rouge_scores = evaluate_model(config=c, model=model_rnn, embedding_model=embedding_model, rouge_maximisation=rouge_maximisation)
         print(rouge_scores)
         rouge_scores_list.append(rouge_scores)
         df = rouge_dict_to_df(rouge_scores)
-        df.to_csv(f"{c['model_name']}_rouge_scores.csv")
+        df.to_csv(f"{c['model_name']}_rouge_scores{str(rouge_maximisation)}.csv")
         # Save the list of dictionaries to a file
-        with open(f"{c['model_name']}_rouge_scores.json", "w") as outfile:
+        with open(f"{c['model_name']}_rouge_scores{str(rouge_maximisation)}.json", "w") as outfile:
             json.dump(rouge_scores_list, outfile)
 
 
@@ -316,19 +360,25 @@ def main():
     # GRU models
     # 64-0.9-None-None
     config = {'model_type': 'gru', 'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv', 'hidden_size': 64,
-              'model_name': 'model-0.001-64-0.9-2023-03-26-19-08.h5'}
+              'model_name': 'model-0.001-64-0.9-2023-04-12-18-30.h5'}
     config['model_path'] = config['model_name']
     configs.append(config)
 
     # 64-0.9-None-dot
     config = {'model_type': 'gru', 'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv', 'hidden_size': 64,
-              'model_name': 'model-0.001-64-0.9-2023-03-26-22-26.h5'}
+              'model_name': 'model-0.001-64-0.9-2023-04-12-17-05.h5'}
+    config['model_path'] = config['model_name']
+    configs.append(config)
+
+    # 64-0.8-fr-None
+    config = {'model_type': 'gru', 'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv', 'hidden_size': 64,
+              'model_name': 'model-0.001-64-0.8-2023-04-13-04-12.h5'}
     config['model_path'] = config['model_name']
     configs.append(config)
 
     # 64-0.8-fr-dot
     config = {'model_type': 'gru', 'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv', 'hidden_size': 64,
-              'model_name': 'model-0.001-64-0.8-2023-03-31-11-08.h5'}
+              'model_name': 'model-0.001-64-0.8-2023-04-12-21-08.h5'}
     config['model_path'] = config['model_name']
     configs.append(config)
 
@@ -337,7 +387,7 @@ def main():
         'seed_v': 42,
         'data_augmentation': 'fr',
         'lr': 2e-5,
-        'training_downsample_rate': 0.75,
+        'training_downsample_rate': 0.8,
         'model_type': 'transformer',
         'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv',
     }
@@ -345,20 +395,33 @@ def main():
     config['model_dir'] = config['model_name']
     configs.append(config)
 
-    evaluate_models(configs=configs, embedding_model=embedding_model, device=device)
+    config = {
+        'seed_v': 42,
+        'data_augmentation': None,
+        'lr': 2e-5,
+        'training_downsample_rate': 0.9,
+        'model_type': 'transformer',
+        'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv',
+    }
+    config[
+        'model_name'] = f"finbert-sentiment-seed-{config['seed_v']}-dataaugm-{config['data_augmentation']}-lr-{config['lr']}-downsample-{config['training_downsample_rate']}"
+    config['model_dir'] = config['model_name']
+    configs.append(config)
 
-    configs = []
-    config = {
-        'model_name': 'lex',
-        'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv',
-    }
-    configs.append(config)
-    config = {
-        'model_name': 'textrank',
-        'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv',
-    }
-    configs.append(config)
-    evaluate_baselines(configs)
+    evaluate_models(configs=configs, embedding_model=embedding_model, rouge_maximisation=True, device=device)
+
+    # configs = []
+    # config = {
+    #     'model_name': 'lex',
+    #     'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv',
+    # }
+    # configs.append(config)
+    # config = {
+    #     'model_name': 'textrank',
+    #     'df_test_path': '../tmp/validation_corpus_2023-02-07 16-33.csv',
+    # }
+    # configs.append(config)
+    # evaluate_baselines(configs)
 
 
 if __name__ == '__main__':
